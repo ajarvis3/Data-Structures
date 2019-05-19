be322c3f8fa8f2d395375a7566e11738f50dad00
Andrew Jarvis <33203802+ajarvis3@users.noreply.github.com>
2019-05-18 23:29:07 -0400

    Revert "Change Stuff. Will revert"
    This reverts commit 40e98add583af5bf41f6a4c4408ad06c03e155f0.

--- LinkedList.h
+++ LinkedList.h
@@ -1,8 +1,6 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
 
-#include <stdlib.h>
-
 // Header file for Linked List
 namespace Linked {
     template <class T>
@@ -50,78 +48,26 @@ namespace Linked {
 
     public:
         // Default constructor.
-        LinkedList() {
-            size = 0;
-            head = nullptr;
-            tail = nullptr;
-        }
-
+        LinkedList();
 
         // Adds a link to the end of the list
-        void add(T* data)  {
-            LinkNode<T>* next = new LinkNode<T>(data);
-            if (tail == nullptr) {
-                head = tail = next;
-            }
-            else {
-                tail->setNext(next);
-                tail = next;
-            }
-            size++;
-        }
+        void add(T data);
 
         // Gets the data at a specified location.
-        T* get(int index) {
-            LinkNode<T>* temp = nullptr;
-            int i = 0;
-            if (index < 0 || index >= size) {
-                return nullptr;
-            }
-            for (temp = head, i = 0; i != index; i++, temp = temp->getNext()) {
-                // Intentionally empty...
-            }
-            return temp->getData();
-        }
+        T get(int index);
 
         // Returns the head LinkNode.
-        LinkNode<T>* getHead() {
-            return head;
-        }
+        LinkNode<T> getHead();
 
         // Checks if the same data is already in the linked list
-        bool contains(T* data) {
-            LinkNode<T>* link;
-            for (link = head; link != nullptr; link = link->getNext()) {
-                T* linkData = link->getData();
-                if (linkData == data || *linkData == *data) {
-                    return true;
-                }
-            }
-            return false;
-        }
+        bool contains(T data);
 
         // Removes the first occurence of a node with relevant data.
         // If a node is removed, return true. Else return false;
-        bool remove(T* data) {
-            LinkNode<T>* trail;
-            LinkNode<T>* lead;
-            for (trail = head; trail != nullptr; trail = lead) {
-                lead = head->getNext();
-                if (head != nullptr && (head->getData() == data)) {
-                    trail->setNext(lead->getNext());
-                    delete lead;
-                    size--;
-                    return true;
-                }
-            }
-            return false;
-        }
+        bool remove(T data);
 
         // Creates and returns an instance of LinkedListIterator for this LinkedList
-        LinkedListIterator<T>* getNewIterator() {
-            LinkedListIterator<T>* iter = new LinkedListIterator<T>(head);
-            return iter;
-        }
+        LinkedListIterator<T>* getNewIterator();
 
     };
 }
--- Tester.cpp
+++ Tester.cpp
@@ -7,18 +7,10 @@ using std::cout;
 int main() {
     LinkedList<int>* list = new LinkedList<int>();
     list->add(10);
-    list->add(10);
-    list->add(100);
-    list->add(5550);
-    list->add(10);
     LinkedListIterator<int>* iter = list->getNewIterator();
     while (iter->hasNext()) {
-        std::cout << ((int)iter->getNext()) << "\n";
+        cout << ((int)iter->getNext());
     }
     delete list;
     return 0;
 }
-
-template class LinkedList<int>;
-template class LinkNode<int>;
-template class LinkedListIterator<int>;
